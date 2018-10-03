2f3254c7ba09f1b1324284ef4f8d56a97a2bf0f3
AJ Magracia <ajmagracia@gmail.com>
2018-10-03 15:36:21 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_181939) do
+ActiveRecord::Schema.define(version: 2018_10_03_183820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "username"
+    t.text "bio"
+    t.text "food_types"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+    t.index ["username"], name: "index_profiles_on_username", unique: true
+  end
+
+  create_table "tips", force: :cascade do |t|
+    t.text "restaurant"
+    t.text "food_type"
+    t.text "description"
+    t.date "date"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tips_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +51,6 @@ ActiveRecord::Schema.define(version: 2018_10_03_181939) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
+  add_foreign_key "tips", "users"
 end
