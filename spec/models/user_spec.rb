c5bca6b21e7543b51282ece7b90247af286c4cd0
AJ Magracia <ajmagracia@gmail.com>
2018-10-04 00:49:44 -0700

    added association tests to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,22 +5,35 @@ RSpec.describe User, type: :model do
 
   describe "Validations" do
     it "is valid with valid attributes" do
-     expect(subject).to be_valid
+      expect(subject).to be_valid
     end
 
     it "is not valid without a password" do
-     subject.password = nil
-     expect(subject).to_not be_valid
+      subject.password = nil
+      expect(subject).to_not be_valid
     end
 
     it "is not valid without a proper password confirmation" do
-     subject.password_confirmation = "some_other_password"
-     expect(subject).to_not be_valid
+      subject.password_confirmation = "some_other_password"
+      expect(subject).to_not be_valid
     end
 
     it "is not valid without an email" do
-     subject.email = nil
-     expect(subject).to_not be_valid
+      subject.email = nil
+      expect(subject).to_not be_valid
     end
   end
+
+  describe "Associations" do
+    it "has one profile" do
+      assc = described_class.reflect_on_association(:profile)
+      expect(assc.macro).to eq :has_one
+    end
+
+    it "has many tips" do
+      assc = described_class.reflect_on_association(:tips)
+      expect(assc.macro).to eq :has_many
+    end
+  end
+
 end
