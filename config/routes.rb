f4422dd80e583e8944cc5f1d6f9cad611ecef1b2
Ryan Higgins <rhiggins32@gmail.com>
2018-10-10 10:06:45 -0700

    added search component and Yelp API call

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :profiles
   resources :tips
   devise_for :users, defaults: { format: :json }
+  post "/search" => "yelp#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
